da2fcc66aa1937630b2a6e3493c46f4776ea90f6
redgradient <neutral1shade@yahoo.com>
2026-07-29 12:02:54 +0400

    refactor: fix too long line

--- review_parser/common_parser/parsers/vkvideo/parser.py
+++ review_parser/common_parser/parsers/vkvideo/parser.py
@@ -15,7 +15,8 @@ def get_token(url: str) -> dict:
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True, args=["--no-sandbox"])
         context = browser.new_context(
-            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/120.0.0.0 Safari/537.36",
             viewport={"width": 1920, "height": 1080},
         )
         page = context.new_page()
