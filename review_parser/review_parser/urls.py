8309978adcce492a6f163762df3555d9d8136cc2
redgradient <neutral1shade@yahoo.com>
2026-07-30 22:44:25 +0400

    refactor(urls): remove legacy API routes

--- review_parser/review_parser/urls.py
+++ review_parser/review_parser/urls.py
@@ -1,16 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from common_parser.views import webhook
-
 from .yasg import urlpatterns as doc_urls
 
 urlpatterns = [  # noqa: RUF005
     path("admin/", admin.site.urls),
-    path("api/yandex/", include("common_parser.urls_common.yandex")),
-    path("api/twogis/", include("common_parser.urls_common.twogis")),
-    path("api/vlru/", include("common_parser.urls_common.vlru")),
     path("api/v1/", include("common_parser.urls")),
-    # path("api/common/", include("common_parser.urls")),
-    path("api/test/webhook", webhook),
 ] + doc_urls
