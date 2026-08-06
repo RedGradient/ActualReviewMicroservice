f504ea237e00b42ac44831b4f8b94b83326138e6
redgradient <neutral1shade@yahoo.com>
2026-08-06 20:01:21 +0400

    feature(admin): add parsing button in Branch view page

--- review_parser/common_parser/admin.py
+++ review_parser/common_parser/admin.py
@@ -1,14 +1,31 @@
 from django.contrib import admin, messages
 from django.shortcuts import redirect
 
-from common_parser.tasks import parse_single_provider
+from common_parser.tasks import parse_branch_providers, parse_single_provider
 
 from .models import Branch, BranchPlatform, Organization, Review
 
 
 @admin.register(Branch)
 class BranchAdmin(admin.ModelAdmin):
+    change_form_template = "admin/common_parser/branch/change_form.html"
     list_display = ("id", "organization", "address", "created_at")
+
+    def change_view(self, request, object_id, form_url="", extra_context=None):
+        if request.method == "POST" and "_parse_all_providers" in request.POST:
+            branch = self.get_object(request, object_id)
+            if branch is None:
+                return redirect("admin:common_parser_branch_changelist")
+
+            task = parse_branch_providers.delay(branch.organization_id, branch.id)
+            self.message_user(
+                request,
+                f"Парсинг всех провайдеров филиала #{branch.id} запущен. task_id={task.id}",
+                level=messages.SUCCESS,
+            )
+            return redirect("admin:common_parser_branch_change", object_id)
+
+        return super().change_view(request, object_id, form_url, extra_context)
 
 
 @admin.register(Organization)
